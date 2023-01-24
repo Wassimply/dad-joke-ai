e096647a5b9ed20d42e5dfd7cabfdfa482b3f87c
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2023-01-25 00:03:59 +0100

    Update popup.js

--- tutorials/hello-world/popup.js
+++ tutorials/hello-world/popup.js
@@ -1,1 +1,13 @@
-console.log("This is a popup!");
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Dad Joke Extension</title>
+  </head>
+  <body>
+    <p>Dad joke about</p>
+    <input type="text" id="topic-input">
+    <button id="submit-button">Submit</button>
+    <div id="result-container"></div>
+    <script src="popup.js"></script>
+  </body>
+</html>
